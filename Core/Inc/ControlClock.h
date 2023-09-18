58f424769102df5e234acc3aecc3b0a0b3544340
huyle0107 <legiahuy01072001@gmail.com>
2023-09-18 15:18:23 +0700

    first commit

---
+++ Core/Inc/ControlClock.h
@@ -0,0 +1,15 @@
+/*
+ * ControlClock.h
+ *
+ *  Created on: Sep 14, 2023
+ *      Author: Admin
+ */
+
+#ifndef INC_CONTROLCLOCK_H_
+#define INC_CONTROLCLOCK_H_
+
+void clearAllClock();
+void setNumberOnClock(int num);
+void clearNumberOnClock(int num);
+
+#endif /* INC_CONTROLCLOCK_H_ */
